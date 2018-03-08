4ed56705c325ec841afaf31e026b5a7e7e3b57f2
Sebastiaan Jansssen <sebastiaan@umbraco.com>
2018-03-08 16:29:54 +0100

    Bump version to 1.0.0

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.0.1.*")]
-[assembly: AssemblyFileVersion("0.0.1-beta")]
-[assembly: AssemblyInformationalVersion("0.0.1-beta")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
