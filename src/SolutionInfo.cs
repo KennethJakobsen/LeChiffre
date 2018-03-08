a679aeda95cb8c76d9dd52742da243494fb60ca8
Sebastiaan Jansssen <sebastiaan@umbraco.com>
2018-03-08 16:11:52 +0100

    Add build scripts

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sebastiaan Janssen")]
+[assembly: AssemblyProduct("LeChiffre")]
+[assembly: AssemblyCopyright("Copyright (c) 2018 - Present")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1-beta")]
+[assembly: AssemblyInformationalVersion("0.0.1-beta")]
